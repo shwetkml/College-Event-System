8b2c74a2ab39002a27396f5939082ef17a93ef60
Shwet Kamal <kamalshwet37@gmail.com>
2022-02-03 10:23:11 +0530

    Initial commit

---
+++ aboutus.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Chiguru 2022</title>
+    <?php require 'utils/styles.php'; ?>
+    <!--css links. file found in utils folder-->
+</head>
+<style>
+/* Large rounded green border */
+hr.blueline {
+    border: 10px solid #00004d;
+    border-radius: 5px;
+}
+
+#bj {
+    font-size: 22px;
+}
+</style>
+
+<body style="background-color:#b3e0ff ">
+
+    <?php require 'utils/header.php'; ?>
+    <hr class="blueline">
+    <div class="col-md-12">
+
+        <h1>About Us</h1>
+
+        <p id="bj"> Cambridge Institute of Technology, affiliated to Visvesvarya Technological University (VTU),
+            Belagavi, Karnataka, approved by All India Council for Technical Education(AICTE) New Delhi, and recognized
+            by the Government of Karnataka.</p>
+
+        <p id="bj">
+            Cambridge Institute Of Technology will provide students with a sense of history, an understanding of the
+            ideals and principles, a
+            commitment to law and morality, an appreciation of human creativity an analytically inquisitive mind. We
+            believe that once the students experience these, they are ready to address the challenges of the rapidly
+            transforming world.</p>
+
+        <p id="bj"> Chiguru is a cultural fest which is held every year, we like everyone to participate cheerfully
+            in every event.</p>
+
+    </div>
+    <hr class="blueline">
+</body>
+
+<?php require 'utils/footer.php'; ?>
+
+</html>
---
+++ utils/footer.php
@@ -0,0 +1,45 @@
+<hr class="footerline">
+<!--css modified horizontal line-->
+<footer>
+    <div class="container">
+        <div class="row">
+            <section>
+                <div class="footerContent col-md-4">
+                    <!--left content-->
+                    <p class="footerContent1">
+                        <strong>C</strong><span class="small footerSubtext">ambridge</span>
+                        <strong>I</strong><span class="small footerSubtext">nstitue of</span>
+                        <strong>T</strong><span class="small footerSubtext">echnology</span>
+                    </p>
+
+                    <p class="footerSubtext2">
+                        Our college Mission is to impart quality technical education and higher moral ethics associated
+                        with skilled training to suit the modern day technology with innovative concepts,so as to learn
+                        to lead the future with full confidence
+                        .
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class="footcontent col-md-4">
+                    <!--middle content-->
+                    <p> <br>
+                        Address: Jai Bhuvaneshwari Layout Rd, SR Layout, Chikkabasavanapura, Krishnarajapura,
+                        <br>Bengaluru, Karnataka 560036
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class="footcontent col-md-4">
+                    <!--right content-->
+                    Follow Us:<br>
+                    <a href="https://www.facebook.com/citech.official/"><img src="images/facebook.png"></a>
+                    <a href="https://www.instagram.com/cit.bengaluru/"><img src="images/instagram.png" width="65px"></a>
+
+                    <a href="https://www.youtube.com/channel/UCXi40Gxq5uUmSGQwz4psM1w"><img
+                            src="images/youtube.png"></a>
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
